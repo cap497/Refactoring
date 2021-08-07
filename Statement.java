991e7a23b4077cd8fd9587a3324eb3cfd7846330
cap497 <63259689+cap497@users.noreply.github.com>
2021-08-07 16:10:07 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,20 @@
-public class Statement {
-  
+import java.util.Enumeration;
+
+public abstract class Statement {
+  public abstract String result(Customer aCustomer);
+  public abstract String incrementResult(String result, Rental each);
+  public abstract String addFooterLines(String result, Customer aCustomer);
+
+  public String value(Customer aCustomer) {
+     Enumeration rentals = aCustomer.getRentals();
+     String result = result(aCustomer);
+     while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        //show figures for this rental
+        result = incrementResult(result,each);
+     }
+     result = addFooterLines(result,aCustomer);
+     
+     return result;
+   }
 }
