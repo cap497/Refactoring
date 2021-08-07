b4970d0502b3eee68d01393552bc5141de567ca1
cap497 <63259689+cap497@users.noreply.github.com>
2021-08-07 16:12:39 -0300

    Commit 18

--- TextStatement.java
+++ TextStatement.java
@@ -1,17 +1,15 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer) {
-     Enumeration rentals = aCustomer.getRentals();
-     String result = "Rental Record for " + aCustomer.getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-        //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
+  public String result(Customer aCustomer) {
+     return "Rental Record for " + aCustomer.getName() + "\n";
+   }
+
+   public String incrementResult(String result, Rental each) {
+     return result + "\t" + each.getMovie().getTitle()+ "\t" +
             String.valueOf(each.getCharge()) + "\n";
-     }
-     //add footer lines
-     result +=  "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
+   }
+
+   public String addFooterLines(String result, Customer aCustomer) {
+     result += "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
      result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
