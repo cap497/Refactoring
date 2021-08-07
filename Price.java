78cc79cd075f526f59be87f3cb42d47b1b006dc7
cap497 <63259689+cap497@users.noreply.github.com>
2021-08-07 08:52:39 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -3,4 +3,14 @@ public abstract class Price {
    
    //determine amounts for each line
    public abstract double getCharge(int daysRented);
+   
+   public int getFrequentRenterPoints(int daysRented){
+    int frequentRenterPoints = 0;
+    // add frequent renter points
+    frequentRenterPoints ++;
+    // add bonus for a two day new release rental
+    if ((getPriceCode() == Movie.NEW_RELEASE) &&
+        daysRented > 1) frequentRenterPoints ++;
+    return frequentRenterPoints;
+  }
 }
