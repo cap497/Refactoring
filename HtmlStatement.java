35e7bc2dbafdd003f588fbdd99f8463bd9f045fe
cap497 <63259689+cap497@users.noreply.github.com>
2021-08-07 16:11:24 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,18 +1,16 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+  public String result(Customer aCustomer) {
+    return "<H1>Rentals for <EM>" + aCustomer.getName() +
       "</EM></H1><P>\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
+  }
+
+  public String incrementResult(String result, Rental each) {
+    return result + each.getMovie().getTitle()+ ": " +
          String.valueOf(each.getCharge()) + "<BR>\n";
-      }
-      //add footer lines
-      result += "<P>You owe <EM>" +
+  }
+
+  public String addFooterLines(String result, Customer aCustomer) {
+     result += "<P>You owe <EM>" +
       String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
       result += "On this rental you earned <EM>" + 
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
